e8033cb8c44364e71cb0cb116afb1a80e48fc77d
longx995 <121871693+longx995@users.noreply.github.com>
2026-03-27 13:24:57 +0800

    Update API keys in envs.js

--- envs.js
+++ envs.js
@@ -14,13 +14,13 @@ export const env = {
   ],
 
   // Google Custom Search API credentials
-  GOOGLE_API_KEY: null,
+  GOOGLE_API_KEY:"AIzaSyDtsUo_0Ep06t84VdkYdayanGYZPYoIyrI",
   GOOGLE_CX: null,
 
   // API 访问令牌 - API access token for authentication
   // 如果设置了 TOKEN，则所有 /search 请求都需要在 header 或 query 中提供此 token
   // If TOKEN is set, all /search requests must provide this token in header or query
-  TOKEN: null,
+  TOKEN: "7f9d2b8e4c6a015378f2e4d6c8b0a25791e3d5b7f9a1c3e5f7b9d1f3a5c7e9b1",
 };
 
 export const setEnv = (newEnv) => {
